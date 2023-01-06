e418084b8b3ef2af5e1540cbdc2ff23fb36ee16b
coppla <65837127+shin-hama@users.noreply.github.com>
2023-01-06 05:55:51 +0000

    FEAT: Add fontawesome to display icons

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,16 +2,19 @@ import * as React from 'react'
 
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
+import SvgIcon from '@mui/material/SvgIcon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <footer>
-      <Stack direction={['row', "column-reverse"]}>
-        <Typography>Copyright 2022 Coppla</Typography>
-        <Box flexGrow={1} />
+      <Stack direction={["column-reverse", "row"]} justifyContent="space-between" alignItems="center">
+        <Typography>Copyright 2022 - Tokyo Cafe Catalog</Typography>
         <Stack direction="row" spacing={2}>
-          <Box>instagram</Box>
+          <SvgIcon>
+            <FontAwesomeIcon icon={faInstagram} />
+          </SvgIcon>
         </Stack>
       </Stack>
     </footer>
