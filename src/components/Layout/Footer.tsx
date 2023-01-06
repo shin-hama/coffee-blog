5edfa4835f0df622efc9c42a393626d6627ebe36
coppla <65837127+shin-hama@users.noreply.github.com>
2023-01-06 05:14:35 +0000

    FEAT: Create footer and layout

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
 
 import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 const Footer = () => {
   return (
     <footer>
-      <Stack direction='row'></Stack>
+      <Stack direction={['row', "column-reverse"]}>
+        <Typography>Copyright 2022 Coppla</Typography>
+        <Box flexGrow={1} />
+        <Stack direction="row" spacing={2}>
+          <Box>instagram</Box>
+        </Stack>
+      </Stack>
     </footer>
   )
 }
