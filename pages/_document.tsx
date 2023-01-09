989d6b9725cd095053e53d31da72c27d4a4d1340
shin-hama <htysivista@gmail.com>
2023-01-09 12:47:40 +0900

    FEAT: Add font link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,18 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin=''
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Noto+Sans+JP:wght@400;700;900&family=Noto+Sans:wght@400;900&family=Roboto:wght@400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
