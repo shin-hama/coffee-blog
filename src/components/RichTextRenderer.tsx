4ae1bdf38d9976c72a6386b37044e1879dd5860c
coppla <65837127+shin-hama@users.noreply.github.com>
2023-01-10 07:08:38 +0000

    FEAT: support typography and hyperlink on richtext by mui

--- src/components/RichTextRenderer.tsx
+++ src/components/RichTextRenderer.tsx
@@ -4,31 +4,66 @@ import {
   Options,
   documentToReactComponents
 } from '@contentful/rich-text-react-renderer'
-import { BLOCKS, Document } from '@contentful/rich-text-types'
+import { BLOCKS, Document, INLINES } from '@contentful/rich-text-types'
+import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 import ContentfulImage from './ContentfulImage'
+import { Link } from './Link'
 
 const renderOption: Options = {
   renderNode: {
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
-      console.log(node)
       return (
-        <ContentfulImage
-          src={node.data.target.fields.file.url}
-          alt={'node.data.target.fields.file.name'}
-          height={node.data.target.fields.file.details.image.height}
-          width={node.data.target.fields.file.details.image.width}
-        />
+        <Box position='relative' width='100%' sx={{ aspectRatio: '16/9' }}>
+          <ContentfulImage
+            src={node.data.target.fields.file.url}
+            alt={node.data.target.fields.title}
+            fill
+            style={{ objectFit: 'contain' }}
+          />
+        </Box>
       )
     },
     [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
       console.log(node)
       console.log(children)
       return <>link to entry</>
+    },
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      return <Typography paragraph>{children}</Typography>
+    },
+    [BLOCKS.HEADING_1]: (node, children) => {
+      return (
+        <Typography component='h1' variant='h1' gutterBottom>
+          {children}
+        </Typography>
+      )
+    },
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return (
+        <Typography component='h2' variant='h2' gutterBottom>
+          {children}
+        </Typography>
+      )
+    },
+    [BLOCKS.HEADING_3]: (node, children) => {
+      return (
+        <Typography component='h3' variant='h3' gutterBottom>
+          {children}
+        </Typography>
+      )
+    },
+    [INLINES.HYPERLINK]: (node, children) => {
+      return <Link href={node.data.uri}>{children}</Link>
+    },
+    [INLINES.ENTRY_HYPERLINK]: (node, children) => {
+      const path = `/${node.data.target.sys.contentType.sys.id}s/${node.data.target.sys.id}`
+
+      return <Link href={path}>{children}</Link>
     }
   },
-  renderText: (text) => <Typography variant='body1'>{text}</Typography>,
+  renderText: undefined,
   renderMark: undefined
 }
 
