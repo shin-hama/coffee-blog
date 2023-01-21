08d78264d9320f38d9b686975bd882d53c37e04f
shin-hama <htysivista@gmail.com>
2023-01-21 09:26:04 +0900

    FEAT: Add next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://www.coffee-junkie.jp',
+  generateRobotsTxt: true,
+  sitemapSize: 7000
+  // ...other options
+}
