80b8d3b8917189af24fd5ea27087971eeb1f3c78
shin-hama <htysivista@gmail.com>
2023-01-22 21:09:13 +0900

    MAINT: Remove server sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,12 +4,6 @@ module.exports = {
   siteUrl: 'https://www.coffee-junkie.jp',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  autoLastmod: false,
-  exclude: ['/server-sitemap.xml', '/cafes/*', '/blogs/*'], // <= exclude here
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://www.coffee-junkie.jp/server-sitemap.xml' // <==== Add here
-    ]
-  }
+  autoLastmod: false
   // ...other options
 }
