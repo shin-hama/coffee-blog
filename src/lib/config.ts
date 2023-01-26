6c965c691e025328814cc1c97c1d05150f32cb4b
shin-hama <htysivista@gmail.com>
2023-01-26 11:30:02 +0900

    MAINT: Create environment for each preview and production build anpit access token and api host

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,9 +2,8 @@ export const isDev = process.env.NODE_ENV === 'development'
 
 export const contentfulSpaceId = process.env.CONTENTFUL_SPACE_ID
 export const contentfulAccessToken = process.env.CONTENTFUL_ACCESS_TOKEN
-export const contentfulPreviewAccessToken =
-  process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
 export const contentfulEnvironment = process.env.CONTENTFUL_ENVIRONMENT
+export const contentfulApiHost = process.env.CONTENTFUL_API_HOST
 
 if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_EMBED_KEY) {
   throw Error('Google Maps Embed API key is empty')
