acb0214e10ea68681fc6c10f4b91749b24effdf6
shin-hama <htysivista@gmail.com>
2023-01-31 10:34:32 +0900

    FEAT: Add oreder query for timestamp

--- src/@types/contentful-query.ts
+++ src/@types/contentful-query.ts
@@ -1,4 +1,5 @@
 export type ContentfulQuery = {
   limit?: number
   skip?: number
+  order?: string
 }
--- src/lib/get-posts.ts
+++ src/lib/get-posts.ts
@@ -10,6 +10,7 @@ export const getAllBlogPosts = async (
 
   const posts = await contentful.getEntries<IBlogFields>({
     content_type: 'blog',
+    order: 'fields.createdAt',
     ...query
   })
 
