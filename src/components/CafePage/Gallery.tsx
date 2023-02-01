83b66e103d05d791f955f29bd7216eef200a922c
coppla <65837127+shin-hama@users.noreply.github.com>
2023-02-01 01:24:32 +0000

    FEAT: Add selected style on thumbnail

--- src/components/CafePage/Gallery.tsx
+++ src/components/CafePage/Gallery.tsx
@@ -2,9 +2,26 @@ import * as React from 'react'
 
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
+import { styled } from '@mui/material/styles'
 import { useSwipeable } from 'react-swipeable'
 
 import ContentfulImage from '../contentful/ContentfulImage'
+
+const Thumbnail = styled(Box)(
+  ({ theme }) => `
+  display: inline-block;
+  padding: 2px;
+  border: 2px solid transparent;
+
+  &:hover {
+    border: 2px solid ${theme.palette.grey[400]};
+  }
+
+  &.selected {
+    border: 2px solid ${theme.palette.text.primary}
+  }
+`
+)
 
 type Props = {
   images: Array<string>
@@ -55,20 +72,24 @@ const Gallery: React.FC<Props> = ({ images }) => {
         </Box>
         <Stack spacing={2} direction='row' overflow={'auto'} height='20%'>
           {images.map((image, i) => (
-            <Box
-              position='relative'
-              height='100%'
+            <Thumbnail
               key={image}
               sx={{ aspectRatio: '1/1' }}
-              onClick={() => setIndex(i)}
+              className={i === index ? 'selected' : ''}
             >
-              <ContentfulImage
-                src={image}
-                alt={''}
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            </Box>
+              <Box
+                height='100%'
+                position='relative'
+                onClick={() => setIndex(i)}
+              >
+                <ContentfulImage
+                  src={image}
+                  alt={''}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </Box>
+            </Thumbnail>
           ))}
         </Stack>
       </Stack>
