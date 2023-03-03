edf425a60ea3ffd5535aa0d5f05a4a3bb188f0e5
shin-hama <htysivista@gmail.com>
2023-03-03 16:52:02 +0900

    MAINT: Wrap nav bar by container to set limit of width

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 
 import { NavLink } from '../Link'
@@ -9,17 +10,19 @@ import { NavLink } from '../Link'
 const Navbar = () => {
   return (
     <Box minWidth='100%' bgcolor={(theme) => theme.palette.grey[100]}>
-      <Stack direction='row' spacing={2}>
-        <Button color='inherit' LinkComponent={NavLink} href='/about'>
-          About
-        </Button>
-        <Button color='inherit' LinkComponent={NavLink} href='/blogs'>
-          Blog
-        </Button>
-        <Button color='inherit' LinkComponent={NavLink} href='/cafes'>
-          Cafe
-        </Button>
-      </Stack>
+      <Container maxWidth='lg'>
+        <Stack direction='row' spacing={2}>
+          <Button color='inherit' LinkComponent={NavLink} href='/about'>
+            About
+          </Button>
+          <Button color='inherit' LinkComponent={NavLink} href='/blogs'>
+            Blog
+          </Button>
+          <Button color='inherit' LinkComponent={NavLink} href='/cafes'>
+            Cafe
+          </Button>
+        </Stack>
+      </Container>
     </Box>
   )
 }
